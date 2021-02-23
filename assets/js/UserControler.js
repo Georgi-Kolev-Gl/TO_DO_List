f99c3a4fa0f78dd1adfd2f327058e0c21003ffd7
Georgi <georgikolev@mail.bg>
2021-02-23 04:40:52 +0200

    added basic print list function

--- assets/js/UserControler.js
+++ assets/js/UserControler.js
@@ -1,6 +1,9 @@
 // getting all elements
 const inputValue = document.getElementById("inputText");
 const addBtn = document.querySelector(".inputField button");
+const todoList = document.querySelector(".todoList");
+const deleteAllBtn = document.querySelector(".removeButton button");
+let listArray = []
 
 //active add button
 inputValue.addEventListener("keyup", function () {
@@ -25,8 +28,45 @@ addBtn.addEventListener("click", function () {
     }
     listArray.push(userEnteredValue);
     localStorage.setItem("New Todo", JSON.stringify(listArray));
+    showTasks(); 
+    addBtn.classList.remove("active");
 })  
 
+function showTasks(){
+  let getLocalStorageData = localStorage.getItem("New Todo");
+  if(getLocalStorageData == null){
+    listArray = [];
+  }else{
+    listArray = JSON.parse(getLocalStorageData); 
+  }
+  if(listArray.length > 0){ 
+    deleteAllBtn.classList.add("active"); 
+  }else{
+    deleteAllBtn.classList.remove("active"); 
+  }
+  let newLiTag = "";
+  listArray.forEach((element, index) => {
+    newLiTag += `<li>${element}<span class="icon" onclick="deleteTask(${index})"><i class="fa fa-trash-o" aria-hidden="true"></i></span></li>`;
+  });
+  todoList.innerHTML = newLiTag; 
+  inputBox.value = ""; 
+}
+
+// delete task function
+function deleteTask(index){
+  let getLocalStorageData = localStorage.getItem("New Todo");
+  listArray = JSON.parse(getLocalStorageData);
+  listArray.splice(index, 1); 
+  localStorage.setItem("New Todo", JSON.stringify(listArray));
+  showTasks(); 
+}
+
+// delete all tasks function
+deleteAllBtn.onclick = ()=>{
+  listArray = []; 
+  localStorage.setItem("New Todo", JSON.stringify(listArray)); 
+  showTasks(); 
+}
 
 
 
